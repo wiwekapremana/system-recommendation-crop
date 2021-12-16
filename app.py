f96e09c88f2552216fcbdf8a78d9daa06611cedc
sintatrisnajayanti <sintatrisnajayanti@gmail.com>
2021-12-16 23:36:07 +0800

    add buttons style

--- app.py
+++ app.py
@@ -9,24 +9,30 @@ import base64
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-b = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+     12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 
-a = ['Apple', 'Banana', 'blackgram', 'chickpea', 'coconut', 'coffee', 'cotton', 'grapes', 'jute', 'kidney beans', 'lentil', 'maize', 'mango', 'moth beans', 'mung bean', 'muskmelon', 'orange', 'papaya', 'pigeonpeas', 'pomegranate', 'Rice', 'Watermelon']
+a = ['Apple', 'Banana', 'blackgram', 'chickpea', 'coconut', 'coffee', 'cotton', 'grapes', 'jute', 'kidney beans', 'lentil',
+     'maize', 'mango', 'moth beans', 'mung bean', 'muskmelon', 'orange', 'papaya', 'pigeonpeas', 'pomegranate', 'Rice', 'Watermelon']
 
 a = pd.DataFrame(a, columns=['label'])
 b = pd.DataFrame(b, columns=['encoded'])
-classes = pd.concat([a,b],axis=1).sort_values('encoded').set_index('label')
+classes = pd.concat([a, b], axis=1).sort_values('encoded').set_index('label')
 
-def predict_proba(n,p,k,temp,humid,pH,rain):
-    data=[[n,p,k,temp,humid,pH,rain]]
+
+def predict_proba(n, p, k, temp, humid, pH, rain):
+    data = [[n, p, k, temp, humid, pH, rain]]
     pred = model.predict_proba(data)
-    pred = pd.DataFrame(data = np.round(pred.T*100,2), index=classes.index,columns=['predicted_values'])
+    pred = pd.DataFrame(data=np.round(pred.T*100, 2),
+                        index=classes.index, columns=['predicted_values'])
     high = pred.predicted_values.nlargest(5)
     return high
+
 
 @app.route('/')
 def welcome():
     return render_template('index.html')
+
 
 @app.route('/', methods=['POST'])
 def predict():
